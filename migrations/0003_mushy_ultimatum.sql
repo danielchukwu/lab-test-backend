2843fda26085d69f08682c69a07623fdf8752ce5
dc <00danzy@gmail.com>
2023-12-19 02:21:51 +0100

    first commit

---
+++ migrations/0003_mushy_ultimatum.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "users" ADD COLUMN "first_name" varchar(100);--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "last_name" varchar(100);--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "date_of_birth" varchar(100);--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "phone" varchar(50);
