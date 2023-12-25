f46367aa3488a19bbb5eb352486c835b2c0d70e5
dc <00danzy@gmail.com>
2023-12-25 23:52:21 +0100

    add auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,13 @@
 import * as authController from "../controllers/authController";
 import { Router } from "express";
+import { requireAuth } from "../middlewares/requireAuth";
 
-const router = Router();
+const authRouter = Router();
 
-router.post('/login', authController.login_post);
-router.post('/signup', authController.signup_post);
-router.post('/forgot-password', authController.forgot_password_post);
-router.post('/reset-password', authController.reset_password_post);
+authRouter.post('/login', authController.login_post);
+authRouter.post('/signup', authController.signup_post);
+authRouter.post('/forgot-password', authController.forgot_password_post);
+authRouter.post('/reset-password', authController.reset_password_post);
+authRouter.post('/change-password/:userId', requireAuth, authController.change_password_post);
 
-export default router;
+export default authRouter;
