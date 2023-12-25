fcc2e4edce9d62ecc5903ab01f1c17c8c73c74e9
dc <00danzy@gmail.com>
2023-12-25 23:50:46 +0100

    complete users table and add gender table

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
-import { integer, pgEnum, pgTable, serial, text, uniqueIndex, uuid, varchar } from 'drizzle-orm/pg-core';
+import { date, integer, pgEnum, pgTable, real, serial, text, uniqueIndex, uuid, varchar } from 'drizzle-orm/pg-core';
+import db from '.';
 
 // declaring enum in database
 // export const popularityEnum = pgEnum('popularity', ['unknown', 'known', 'popular']);
@@ -22,13 +23,31 @@ import { integer, pgEnum, pgTable, serial, text, uniqueIndex, uuid, varchar } fr
 export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
   avatar: varchar('avatar', {length: 255}),
-  first_name: varchar('first_name', {length: 100}),
-  last_name: varchar('last_name', {length: 100}),
-  date_of_birth: varchar('date_of_birth', {length: 100}),
-  phone: varchar('phone', { length: 50 }),
-
-  email: varchar('email', {length: 100}).unique().notNull(),
+  full_name: varchar('first_name', {length: 30}).notNull(),
+  email: varchar('email', {length: 50}).unique().notNull(),
+  gender_id: uuid('gender').notNull().references(() => gender.id),
   password: varchar('password', {length: 256}).notNull(),
+  phone_number: varchar('phone_number', { length: 50 }).notNull(),
+  date_of_birth: varchar('date_of_birth').notNull(),
+  
+  // address information
+  street_address: varchar('street_address', { length: 200 }),
+  city: varchar('city', { length: 50 }),
+  state: varchar('state', { length: 50 }),
+  country: varchar('country', { length: 50 }),
+  
+  // medical information
+  allergies: varchar('allergies', { length: 50 }),
+  height: real('height'),
+  weight: real('weight'),
+  current_medications: varchar('current_medications', { length: 50 }),
+  
 });
+
+export const gender = pgTable('gender', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  value: varchar('gender', { length: 10, enum: ['male', 'female'] }).unique(),
+});
+
 export type User = typeof users.$inferSelect; // return type when queried
 export type NewUser = typeof users.$inferInsert; // insert type
