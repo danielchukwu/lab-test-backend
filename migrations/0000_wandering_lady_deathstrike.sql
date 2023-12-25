2d636c7a39316328ec489c22be5b6a28a6484a61
dc <00danzy@gmail.com>
2023-12-25 23:39:51 +0100

    remove

--- migrations/0000_wandering_lady_deathstrike.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"avatar" varchar(255),
-	"first_name" varchar(100),
-	"last_name" varchar(100),
-	"date_of_birth" varchar(100),
-	"phone" varchar(50),
-	"email" varchar(50)
-);
--- migrations/0003_mushy_ultimatum.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "first_name" varchar(100);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "last_name" varchar(100);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "date_of_birth" varchar(100);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "phone" varchar(50);
